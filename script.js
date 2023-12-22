8ec7579af9ac27ef29c467caff7dbdd9414fca23
Adeel-rauf <153929392+Adeel-rauf@users.noreply.github.com>
2023-12-22 11:08:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+let user = prompt('enter among them: snake, water, gun');
+let random = Math.round(Math.random() * 2);
+let cpu = ['snake', 'water', 'gun'][random];
+const match = (user, cpu) => {
+    if (user == cpu) {
+        return 'Nobody, both bite the dust';
+    }
+    else if (user == 'snake' && cpu == 'gun') {
+        return 'cpu';
+    }
+    else if (user == 'gun' && cpu == 'water') {
+        return 'cpu';
+    }
+    else if (user == 'water' && cpu == 'snake') {
+        return 'cpu';
+    }
+    else if (user == 'snake' && cpu == 'water') {
+        return 'user';
+    }
+    else if (user == 'gun' && cpu == 'snake') {
+        return 'user';
+    }
+    else if (user == 'water' && cpu == 'gun') {
+        return 'user';
+    }
+};
+let result = match(user, cpu);
+let r = document.getElementsByClassName('res')[0];
+r.innerHTML = (`${result}`);
+let x = document.getElementsByClassName('ann')[0];
+x.innerHTML = (`user with \"${user}\" & cpu with \"${cpu}\"fought`);
+// document.write(`user with ${user} & cpu with ${cpu} fought`)
